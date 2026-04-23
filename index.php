6dbee7f1c567a999bccd45fe3b3671be89d5c07a
Kroth09 <eduardokroth10@gmail.com>
2026-04-23 15:37:25 -0300

    a

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CineTrack</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body>
+<header class=" w-full bg-red-700 text-white">
+    <nav class="px-4">
+
+        <ul class="flex w-full justify-between items-center py-4">
+
+                <div>CineTrack</div>
+
+                <ul class="flex gap-6">
+                    <li><a href="/home" class="hover:underline">Explorar</a></li>
+                    <li><a href="/meus-filmes" class="hover:underline">Meus Filmes</a></li>
+                    <li><a href="/logout" class="hover:underline">(Usuario)</a></li>
+                </ul>
+
+        </ul>
+
+    </nav>
+</header>
+
+
+
+
+</body>
+</html>
